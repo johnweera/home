f88a48e84c1b0960e7d34a2fc177ec419cf8ff1c
johnweera <johnweera.c@gmail.com>
2024-05-29 22:21:16 +0700

    home

---
+++ static/home/js/widgets.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.getElementById("python").innerHTML = "Python";
+// })
+
+
+document.getElementById("python").innerHTML = "Python";
