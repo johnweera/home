ffe1c3b766ee9a2654ed950f27557f314161fbdb
johnweera <johnweera.c@gmail.com>
2024-10-25 20:02:47 +0700

    second

--- views.py
+++ views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'home/index.html')
-
-def toggle(request):
-    return render(request, 'home/toggle.html')
